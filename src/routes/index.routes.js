08481a638baf483955c8be111ee0014c84a31b89
joao vitor diniz mistrinel <abissaldev@gmail.com>
2023-07-12 14:45:41 -0300

    add joi uuid

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,8 @@
 import { Router } from "express"
 import authRouter from "./auth.routes.js"
-import transactionRouter from "./transaction.routes.js"
 
 const router = Router()
 
 router.use(authRouter)
-router.use(transactionRouter)
 
 export default router
