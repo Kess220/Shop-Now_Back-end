5f0547a65af2a2da61f771916229bb2b9b615eb8
joao vitor diniz mistrinel <abissaldev@gmail.com>
2023-07-12 15:04:02 -0300

    Add image URL validation to userSchema

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -3,10 +3,12 @@ import Joi from "joi"
 export const userSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(3).required()
+    password: Joi.string().min(3).required(),
+    image: Joi.string().uri({ scheme: ['http', 'https'] }).optional()
 })
 
 export const loginSchema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().min(3).required()
 })
+
