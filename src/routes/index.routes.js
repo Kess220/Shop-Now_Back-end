6889631a9a7ea5ba46a65a9dc2e5120b2354170e
MariCMelo <marilia.correa.melo@gmail.com>
2023-07-13 00:08:34 -0300

    mod indexRoutes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import authRouter from "./auth.routes.js"
+import cartRouter from "./cart.routes.js"
 
 const router = Router()
 
 router.use(authRouter)
+router.use(cartRouter)
 
 export default router
