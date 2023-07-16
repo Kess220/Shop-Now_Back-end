cfd84ac1bc5134220dcaaa501a39550799853533
João Vitor Diniz Mistrinel <abissaldev@gmail.com>
2023-07-16 15:29:04 -0300

    add /profile/routes

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,17 +1,82 @@
 import { db } from "../database/db.connection.js"
+import bcrypt from "bcrypt"
+import { ObjectId } from "mongodb";
+
 
 export async function getProfile(req, res) {
-  const token = res.locals.session.token
+  const userId = req.params.userId;
+
   try {
-      const session = await db.collection("sessions").findOne({ token })
-      if (!session) return res.status(401).send("Token inválido")
+    const user = await db.collection("users").findOne({ _id: new ObjectId(userId) });
+    if (!user) return res.status(404).send("Usuário não encontrado");
 
-      const user = await db.collection("users").findOne({ _id: session.userId })
-      if (!user) return res.status(404).send("Usuário não encontrado")
-
-      res.send({ username: user.name, email: user.email })
+    res.send({ username: user.name, email: user.email });
 
   } catch (err) {
-      res.status(500).send(err.message)
+    res.status(500).send(err.message);
   }
 }
+
+export async function changePassword(req, res) {
+  const userId = req.params.userId;
+  const { currentPassword, newPassword } = req.body;
+
+  try {
+    const user = await db.collection("users").findOne({ _id: new ObjectId(userId) });
+    if (!user) return res.status(404).send("Usuário não encontrado");
+
+    const isPasswordCorrect = bcrypt.compareSync(currentPassword, user.password);
+    if (!isPasswordCorrect) return res.status(401).send("Senha atual incorreta");
+
+    const newHash = bcrypt.hashSync(newPassword, 10);
+
+    await db.collection("users").updateOne({ _id: new ObjectId(userId) }, { $set: { password: newHash } });
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function changeName(req, res) {
+  const userId = req.params.userId;
+  const { newName } = req.body;
+
+  try {
+    await db.collection("users").updateOne({ _id: new ObjectId(userId) }, { $set: { name: newName } });
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function changeEmail(req, res) {
+  const userId = req.params.userId;
+  const { newEmail } = req.body;
+
+  try {
+    const existingUser = await db.collection("users").findOne({ email: newEmail });
+    if (existingUser) return res.status(409).send("E-mail já está sendo utilizado por outro usuário");
+
+    await db.collection("users").updateOne({ _id: new ObjectId(userId) }, { $set: { email: newEmail } });
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function changeImage(req, res) {
+  const userId = req.params.userId;
+  const { newImage } = req.body;
+
+  try {
+    await db.collection("users").updateOne({ _id: new ObjectId(userId) }, { $set: { image: newImage } });
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { authValidation } from "../middlewares/authValidation.js"
-import { getProfile } from "../controllers/profile.controller.js"
+import { getProfile, changeName, changeEmail, changePassword, changeImage } from "../controllers/profile.controller.js"
 
 const profileRouter = Router()
 
-profileRouter.get("/profile", authValidation, getProfile); 
+profileRouter.get("/profile/:userId", authValidation, getProfile);
+profileRouter.put("/profile/update-name/:userId", authValidation, changeName);
+profileRouter.put("/profile/update-email/:userId", authValidation, changeEmail);
+profileRouter.put("/profile/update-password/:userId", authValidation, changePassword);
+profileRouter.put("/profile/update-image/:userId", authValidation, changeImage);
+
 
 export default profileRouter
-
-
-
