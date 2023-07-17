9b65eb4e75f730a64a3465f728ed1755f4cc7bf9
unknown <prodvictorkaio@gmail.com>
2023-07-17 01:34:10 -0300

    a

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import express from "express"
-import cors from "cors"
-import router from "./routes/index.routes.js"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.routes.js";
+import dotenv from "dotenv";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(router)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
